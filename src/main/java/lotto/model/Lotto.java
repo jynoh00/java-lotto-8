54aacbf84939d21071bc35c10bdca4fb5c4d5367
jynoh00 <wndus123sh@naver.com>
2025-10-29 07:12:30 +0900

    feat(Lotto): 개별 인스턴스에서 로또 번호 출력 기능 추가

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,4 +1,4 @@
-package lotto;
+package lotto.model;
 
 import java.util.List;
 
@@ -14,6 +14,10 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+    }
+
+    public String getNumbers() {
+        return numbers.toString(); // 미구현
     }
 
     // TODO: 추가 기능 구현
