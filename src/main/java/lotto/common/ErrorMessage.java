7f7682cdb84f65ee3312494a26f78bfed585dd70
jynoh00 <wndus123sh@naver.com>
2025-10-29 03:22:45 +0900

    feat(ErrorMessage): 예외 처리 메시지 Enum 구현

---
+++ src/main/java/lotto/common/ErrorMessage.java
@@ -0,0 +1,22 @@
+package lotto.common;
+
+public enum ErrorMessage {
+    INVALID_NUMBER_FORMAT("올바르지 않은 숫자 형식입니다."),
+    MUST_BE_POSITIVE("입력한 숫자는 양의 정수여야 합니다."),
+    MUST_BE_DIVISIBLE_BY_THOUSEND("구입금액은 1,000원으로 나누어 떨어져야 합니다."),
+    EMPTY_INPUT("입력값이 비어있습니다."),
+    INVALID_WINNING_NUMBERS_FORMAT("당첨번호는 숫자, 쉼표, 공백으로만 구성되어야 합니다."),
+    INVALID_LOTTO_NUMBER_RANGE("로또 숫자는 1~45 내 정수여야 합니다.");
+
+
+    private static final String ERROR_PREFIX = "[ERROR] ";
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return ERROR_PREFIX + message;
+    }
+}
