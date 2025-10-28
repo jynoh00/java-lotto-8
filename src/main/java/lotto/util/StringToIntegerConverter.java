e30508e3f028d6557db4ffdad4250348f5e7e57c
jynoh00 <wndus123sh@naver.com>
2025-10-29 07:21:52 +0900

    feat(StringToIntegerConverter): 숫자 형식 문자열 Integer타입 변환 유틸리티 구현

---
+++ src/main/java/lotto/util/StringToIntegerConverter.java
@@ -0,0 +1,13 @@
+package lotto.util;
+
+import lotto.common.ErrorMessage;
+
+public class StringToIntegerConverter {
+    public static int convert(String inputNumber) {
+        try {
+            return Integer.parseInt(inputNumber.trim());
+        }catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_FORMAT.getMessage());
+        }
+    }
+}
