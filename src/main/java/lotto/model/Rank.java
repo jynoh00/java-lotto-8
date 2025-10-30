6b8c3281404c7e54578978ab84c3d5f468533be2
jynoh00 <wndus123sh@naver.com>
2025-10-30 14:04:34 +0900

    feat(Rank): 중복 숫자 개수별 랭크 지정 Enum 추가

---
+++ src/main/java/lotto/model/Rank.java
@@ -0,0 +1,34 @@
+package lotto.model;
+
+public enum Rank {
+    FIRST(6, 2_000_000_000, false),
+    SECOND(5, 30_000_000, true),
+    THIRD(5, 1_500_000, false),
+    FOURTH(4, 50_000, false),
+    FIFTH(3, 5_000, false),
+    NONE(0, 0, false);
+
+    private final int matchCount;
+    private final int prize;
+    private final boolean matchBonus;
+
+    Rank(int matchCount, int prize, boolean matchBonus) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+        this.matchBonus = matchBonus;
+    }
+
+    public static Rank valueOf(int matchCount, boolean matchBonus) {
+        if (matchCount == 6) return FIRST;
+        if (matchCount == 5 && matchBonus) return SECOND;
+        if (matchCount == 5) return THIRD;
+        if (matchCount == 4) return FOURTH;
+        if (matchCount == 3) return FIFTH;
+
+        return NONE;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+}
