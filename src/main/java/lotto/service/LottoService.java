febb6a62a167d3d45dd9b19cf4272a761135f163
jynoh00 <wndus123sh@naver.com>
2025-10-30 14:07:05 +0900

    feat(LottoService): 랭크 기반 통계 계산 기능 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -3,11 +3,15 @@ package lotto.service;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import lotto.common.LottoConstants;
+import lotto.model.Lotto;
 import lotto.model.LottoSimulator;
+import lotto.model.Rank;
 import lotto.model.Statistics;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LottoService {
     public LottoSimulator createSimulator(int purchasePrice, List<Integer> winningNumbers, int bonusNumber) {
@@ -17,7 +21,34 @@ public class LottoService {
     }
 
     public Statistics calculateStatistics(LottoSimulator simulator) {
-        return new Statistics(); // 미구현
+        Map<Rank, Integer> rankCounts = new HashMap<>();
+        for (Rank rank : Rank.values()) {
+            rankCounts.put(rank, 0);
+        }
+
+        for (Lotto lotto : simulator.getPurchasedLottos()) {
+            Rank rank = calculateRank(lotto, simulator.getWinningNumbers(), simulator.getBonusNumber());
+            rankCounts.put(rank, rankCounts.get(rank) + 1);
+        }
+
+        int totalPurchaseAmount = simulator.getPurchaseCount() * LottoConstants.LOTTO_PRICE.getValue();
+        return new Statistics(rankCounts, totalPurchaseAmount); // 미구현
+    }
+
+    private Rank calculateRank(Lotto lotto, List<Integer> winningNumbers, int bonusNumber) {
+        int matchCount = countMatches(lotto.getNumbers(), winningNumbers);
+        boolean matchBonus = lotto.getNumbers().contains(bonusNumber);
+
+        return Rank.valueOf(matchCount, matchBonus);
+    }
+
+    private int countMatches(List<Integer> numbers, List<Integer> winningNumbers) {
+        int count = 0;
+        for (int number : numbers) {
+            if (winningNumbers.contains(number)) count++;
+        }
+
+        return count;
     }
 
     public List<Integer> makeLottoNumbers() {
