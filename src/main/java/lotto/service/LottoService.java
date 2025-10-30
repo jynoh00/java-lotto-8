93cad0823191c549155e8ced520aba0edb9be976
jynoh00 <wndus123sh@naver.com>
2025-10-30 12:00:49 +0900

    feat(LottoService): 자동 로또 번호 리스트 생성 기능 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,16 +1,37 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import lotto.common.LottoConstants;
 import lotto.model.LottoSimulator;
 import lotto.model.Statistics;
 
+import java.util.Collections;
 import java.util.List;
 
 public class LottoService {
     public LottoSimulator createSimulator(int purchasePrice, List<Integer> winningNumbers, int bonusNumber) {
-        return new LottoSimulator(); // 미구현
+        int purchaseCount = getPurchaseCount(purchasePrice);
+
+        return new LottoSimulator(purchaseCount, winningNumbers, bonusNumber);
     }
 
     public Statistics calculateStatistics(LottoSimulator simulator) {
         return new Statistics(); // 미구현
+    }
+
+    public List<Integer> makeLottoNumbers() {
+        List<Integer> lottoNumbers = Randoms.pickUniqueNumbersInRange(
+                LottoConstants.LOTTO_MIN_NUMBER.getValue(),
+                LottoConstants.LOTTO_MAX_NUMBER.getValue(),
+                LottoConstants.LOTTO_NUMBERS_LENGTH.getValue()
+        );
+        Collections.sort(lottoNumbers);
+
+        return lottoNumbers;
+    }
+
+    private int getPurchaseCount(int purchasePrice) {
+        return purchasePrice / LottoConstants.LOTTO_PRICE.getValue();
     }
 }
