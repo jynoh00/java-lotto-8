f1f321d697992e4a8b25a0dae6f93d6cf6d898f5
jynoh00 <wndus123sh@naver.com>
2025-10-30 14:02:03 +0900

    feat(Statistics): 랭크 기반 수익률 추출 메서드 추가

--- src/main/java/lotto/model/Statistics.java
+++ src/main/java/lotto/model/Statistics.java
@@ -1,5 +1,34 @@
 package lotto.model;
 
+import java.util.Map;
+
 public class Statistics {
-    // 미구현
+    private final Map<Rank, Integer> rankCounts;
+    private final double outputRate;
+
+    public Statistics(Map<Rank, Integer> rankCounts, int purchasePrice) {
+        this.rankCounts = rankCounts;
+        this.outputRate = calculateOutputRate(rankCounts, purchasePrice);
+    }
+
+    private double calculateOutputRate(Map<Rank, Integer> rankCounts, int purchasePrice) {
+        long totalPrize = 0;
+        for (Map.Entry<Rank, Integer> entry : rankCounts.entrySet()) {
+            Rank rank = entry.getKey();
+            int count = entry.getValue();
+            if (rank != Rank.NONE) {
+                totalPrize += rank.getPrize() * count;
+            }
+        }
+
+        return (double) totalPrize / purchasePrice * 100;
+    }
+
+    public double getOutputRate() {
+        return outputRate;
+    }
+
+    public int getCountByRank(Rank rank) {
+        return rankCounts.getOrDefault(rank, 0);
+    }
 }
