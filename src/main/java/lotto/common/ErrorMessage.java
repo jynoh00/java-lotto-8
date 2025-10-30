cdc3cbed223eb3cb5292bf09a653d1dbe39cdabe
jynoh00 <wndus123sh@naver.com>
2025-10-30 20:30:29 +0900

    feat(ErrorMessage): 에러 상황 추가

--- src/main/java/lotto/common/ErrorMessage.java
+++ src/main/java/lotto/common/ErrorMessage.java
@@ -8,7 +8,8 @@ public enum ErrorMessage {
     INVALID_WINNING_NUMBERS_FORMAT("로또 번호는 숫자, 쉼표, 공백으로만 구성되어야 합니다."),
     INVALID_LOTTO_NUMBER_RANGE("로또 숫자는 1~45 내 정수여야 합니다."),
     INVALID_LOTTO_NUMBERS_LENGTH("로또 번호의 개수는 6개여야 합니다."),
-    DUPLICATE_LOTTO_NUMBER("로또 숫자는 중복되지 않아야 합니다.");
+    DUPLICATE_LOTTO_NUMBER("로또 숫자는 중복되지 않아야 합니다."),
+    BONUS_NUMBER_REDUNDANCY("보너스 숫자는 로또 번호와 중복되지 않아야 합니다.");
 
 
     private static final String ERROR_PREFIX = "[ERROR] ";
